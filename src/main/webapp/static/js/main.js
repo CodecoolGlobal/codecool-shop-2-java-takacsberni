0d2c3914888dae6954c350664eabd9b6eb372186
Hanna Szepesi <hannaszepesi.7@gmail.com>
2022-05-03 15:17:25 +0200

    feat: merge

--- src/main/webapp/static/js/main.js
+++ src/main/webapp/static/js/main.js
@@ -1,3 +1,14 @@
+let categoriesDropdown = document.getElementById("categoriesDropdownDiv");
+let suppliersDropdown = document.getElementById("suppliersDropdownDiv");
+
+for(let category of categoriesDropdown.children){
+    category.addEventListener('click', () => alert('Category clicked!'));
+}
+
+for(let supplier of suppliersDropdown.children){
+    supplier.addEventListener('click', () => alert('Supplier clicked!'));
+}
+
 let init = {
     getProductsBySupplier: async function (supplierId) {
         let url = "/supplier/" + "?supplier_id=" + supplierId;
@@ -10,5 +21,4 @@ let init = {
         return response.json();
     }
 }
-
 

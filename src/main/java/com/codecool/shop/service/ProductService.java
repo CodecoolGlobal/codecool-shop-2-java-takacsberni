080bc7dd80390fef41c9616a0b13d7d1b0287ba9
DianaDob <dobsinszki.diana@gmail.com>
2022-05-03 13:55:04 +0200

    feat: add suppliers to dropdown list with data-id = supplier id

--- src/main/java/com/codecool/shop/service/ProductService.java
+++ src/main/java/com/codecool/shop/service/ProductService.java
@@ -2,18 +2,22 @@ package com.codecool.shop.service;
 
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.SupplierDao;
 import com.codecool.shop.model.Product;
 import com.codecool.shop.model.ProductCategory;
+import com.codecool.shop.model.Supplier;
 
 import java.util.List;
 
 public class ProductService{
     private ProductDao productDao;
     private ProductCategoryDao productCategoryDao;
+    private SupplierDao supplierDao;
 
-    public ProductService(ProductDao productDao, ProductCategoryDao productCategoryDao) {
+    public ProductService(ProductDao productDao, ProductCategoryDao productCategoryDao, SupplierDao supplierDao) {
         this.productDao = productDao;
         this.productCategoryDao = productCategoryDao;
+        this.supplierDao = supplierDao;
     }
 
     public ProductCategory getProductCategory(int categoryId){
@@ -27,5 +31,9 @@ public class ProductService{
 
     public List<ProductCategory> getAllCategories(){
         return productCategoryDao.getAll();
+    }
+
+    public List<Supplier> getAllSuppliers() {
+        return supplierDao.getAll();
     }
 }

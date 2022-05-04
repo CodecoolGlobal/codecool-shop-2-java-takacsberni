02107fefb107688d92a09dd9be891e12fc188004
Hanna Szepesi <hannaszepesi.7@gmail.com>
2022-05-04 11:51:11 +0200

    fix: add all categories and suppliers list to context

--- src/main/java/com/codecool/shop/controller/CategoryController.java
+++ src/main/java/com/codecool/shop/controller/CategoryController.java
@@ -3,9 +3,12 @@ package com.codecool.shop.controller;
 import com.codecool.shop.config.TemplateEngineUtil;
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.SupplierDao;
 import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
 import com.codecool.shop.dao.implementation.ProductDaoMem;
+import com.codecool.shop.dao.implementation.SupplierDaoMem;
 import com.codecool.shop.service.ProductService;
+import com.codecool.shop.service.SupplierService;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -24,13 +27,17 @@ public class CategoryController extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         ProductDao productDataStore = ProductDaoMem.getInstance();
         ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
+        SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
         ProductService productService = new ProductService(productDataStore, productCategoryDataStore);
+        SupplierService supplierService = new SupplierService(supplierDataStore);
 
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
         String categoryId = (req.getParameter("category_id"));
         context.setVariable("category", productService.getProductCategory(Integer.parseInt(categoryId)));
         context.setVariable("products", productService.getProductsForCategory(Integer.parseInt(categoryId)));
+        context.setVariable("all_categories", productService.getAllCategories());
+        context.setVariable("all_suppliers", supplierService.getAllSuppliers());
         // // Alternative setting of the template context
         // Map<String, Object> params = new HashMap<>();
         // params.put("category", productCategoryDataStore.find(1));

94829aa54a83bd1b9f674a416003785773ed2617
takacsberni <takacsberni@gmail.com>
2022-05-05 11:51:31 +0200

    feat: start to fill edit_cart with data

--- src/main/java/com/codecool/shop/controller/EditCartController.java
+++ src/main/java/com/codecool/shop/controller/EditCartController.java
@@ -1,12 +1,16 @@
 package com.codecool.shop.controller;
 
 import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.dao.OrderDao;
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
 import com.codecool.shop.dao.SupplierDao;
+import com.codecool.shop.dao.implementation.OrderDaoMem;
 import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
 import com.codecool.shop.dao.implementation.ProductDaoMem;
 import com.codecool.shop.dao.implementation.SupplierDaoMem;
+import com.codecool.shop.model.LineItem;
+import com.codecool.shop.service.OrderService;
 import com.codecool.shop.service.ProductService;
 import com.codecool.shop.service.SupplierService;
 import org.thymeleaf.TemplateEngine;
@@ -18,9 +22,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(urlPatterns = {"/edit_cart"})
 public class EditCartController extends HttpServlet {
+    OrderDao orderDataStore = OrderDaoMem.getInstance();
+    OrderService orderservice = new OrderService(orderDataStore);
+
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         ProductDao productDataStore = ProductDaoMem.getInstance();
@@ -29,9 +38,14 @@ public class EditCartController extends HttpServlet {
         ProductService productService = new ProductService(productDataStore,productCategoryDataStore, supplierDataStore);
         SupplierService supplierService = new SupplierService(supplierDataStore);
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+
+        List<LineItem> items = orderservice.getLineItems(1);
+        int lineItemNumber = orderservice.getLineItems(1).size();
+
         WebContext context = new WebContext(req, resp, req.getServletContext());
         context.setVariable("all_categories", productService.getAllCategories());
         context.setVariable("all_suppliers", supplierService.getAllSuppliers());
+        context.setVariable("items", items);
         engine.process("product/edit_cart.html", context, resp.getWriter());
 
     }

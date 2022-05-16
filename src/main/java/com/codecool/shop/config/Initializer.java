f0336d46e08ff6bfcb21dde1a93f24a86cb0e717
takacsberni <takacsberni@gmail.com>
2022-05-16 21:16:46 +0200

    feat: Initializer now can handle if jdbc or memory load

--- src/main/java/com/codecool/shop/config/Initializer.java
+++ src/main/java/com/codecool/shop/config/Initializer.java
@@ -19,59 +19,78 @@ import javax.servlet.annotation.WebListener;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.sql.SQLException;
+import java.util.Objects;
 
 @WebListener
 public class Initializer implements ServletContextListener {
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-        DatabaseManager databaseManager = new DatabaseManager();
+        DatabaseManager databaseManager = DatabaseManager.getInstance();
+        ProductDao productDataStore = null;
+        SupplierDao supplierDataStore = null;
+        ProductCategoryDao productCategoryDataStore = null;
+        OrderDaoMem orderDataStore = null;
+
         try {
             databaseManager.setup();
         } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            System.out.println("File not found");
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("Input/Output error");
+        } catch (SQLException e) {
+            System.out.println("Can't connect to the database.");
         }
 
 
-        ProductDao productDataStore = ProductDaoMem.getInstance();
-        ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
-        SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
-        OrderDaoMem orderDataStore = OrderDaoMem.getInstance();
+        String daoProperty = databaseManager.getProperties().getProperty("dao");
 
-        int orderId = orderDataStore.getOrders().size()+1;
-        Order newOrder = new Order(orderId);
-        orderDataStore.add(newOrder);
+        if (Objects.equals(daoProperty, "jdbc")){
+            productDataStore = databaseManager.getProductDao();
+            supplierDataStore = databaseManager.getSupplierDataStore();
+            productCategoryDataStore = databaseManager.getProductCategoryDataStore();
+            orderDataStore = databaseManager.getOrderDataStore();
+        }
+        else if (Objects.equals(daoProperty, "memory")){ //EZEK A KORÁBBI RÉSZEK VÁLTOZTATÁS NÉLKÜL
+            productDataStore = ProductDaoMem.getInstance();
+            productCategoryDataStore = ProductCategoryDaoMem.getInstance();
+            supplierDataStore = SupplierDaoMem.getInstance();
+            orderDataStore = OrderDaoMem.getInstance();
 
-        //setting up a new supplier
-        Supplier chernobylPetShop = new Supplier("Chernobyl Pet Shop", "Exotic animals from Chernobyl");
-        supplierDataStore.add(chernobylPetShop);
-        Supplier petSuppliesPlus = new Supplier("Pet Supplies Plus", "Pet Supplies Plus is your local pet store carrying a wide variety of pets");
-        supplierDataStore.add(petSuppliesPlus);
-        Supplier superPetStore = new Supplier("Super Pet Store", "Super Pet Store is the best choice for pocket pets!");
-        supplierDataStore.add(superPetStore);
-        Supplier smallPetSelect = new Supplier("Small Pet Select", "Browse our small animals and discover the joy of bringing home one of the many pets for sale!");
-        supplierDataStore.add(smallPetSelect);
+            int orderId = orderDataStore.getOrders().size()+1;
+            Order newOrder = new Order(orderId);
+            orderDataStore.add(newOrder);
 
-        //setting up a new product category
-        ProductCategory cat = new ProductCategory("Cat", "Animals", "The cat is a domestic species of small carnivorous mammal.");
-        productCategoryDataStore.add(cat);
-        ProductCategory reptiles = new ProductCategory("Reptiles", "Animals", "Reptiles are air-breathing vertebrates covered in special skin made up of scales, bony plates, or a combination of both.");
-        productCategoryDataStore.add(reptiles);
-        ProductCategory pocketPets = new ProductCategory("Pocket Pets", "Animals", "Pocket Pet refers to small animals such as guinea pigs, hamsters, hedgehogs, mice, rats, gerbils, chinchillas, and sugar gliders.");
-        productCategoryDataStore.add(pocketPets);
-        ProductCategory smallMammals = new ProductCategory("Small Mammals", "Animals", "Small mammals include a few animals that are not so small, such as rabbits and prairie dogs.");
-        productCategoryDataStore.add(smallMammals);
+            //setting up a new supplier
+            Supplier chernobylPetShop = new Supplier("Chernobyl Pet Shop", "Exotic animals from Chernobyl");
+            supplierDataStore.add(chernobylPetShop);
+            Supplier petSuppliesPlus = new Supplier("Pet Supplies Plus", "Pet Supplies Plus is your local pet store carrying a wide variety of pets");
+            supplierDataStore.add(petSuppliesPlus);
+            Supplier superPetStore = new Supplier("Super Pet Store", "Super Pet Store is the best choice for pocket pets!");
+            supplierDataStore.add(superPetStore);
+            Supplier smallPetSelect = new Supplier("Small Pet Select", "Browse our small animals and discover the joy of bringing home one of the many pets for sale!");
+            supplierDataStore.add(smallPetSelect);
+
+            //setting up a new product category
+            ProductCategory cat = new ProductCategory("Cat", "Animals", "The cat is a domestic species of small carnivorous mammal.");
+            productCategoryDataStore.add(cat);
+            ProductCategory reptiles = new ProductCategory("Reptiles", "Animals", "Reptiles are air-breathing vertebrates covered in special skin made up of scales, bony plates, or a combination of both.");
+            productCategoryDataStore.add(reptiles);
+            ProductCategory pocketPets = new ProductCategory("Pocket Pets", "Animals", "Pocket Pet refers to small animals such as guinea pigs, hamsters, hedgehogs, mice, rats, gerbils, chinchillas, and sugar gliders.");
+            productCategoryDataStore.add(pocketPets);
+            ProductCategory smallMammals = new ProductCategory("Small Mammals", "Animals", "Small mammals include a few animals that are not so small, such as rabbits and prairie dogs.");
+            productCategoryDataStore.add(smallMammals);
 
 
-        //setting up products and printing it
-        productDataStore.add(new Product("Bear Cat", new BigDecimal("10000"), "USD", "This bear cat is an authentic animal from Chernobyl, might still radiate.", cat, chernobylPetShop));
-        productDataStore.add(new Product("Rabbit Cat", new BigDecimal("3000"), "USD", "The rabbit cat is the nicest little thing caused by the accident. (only if you don't mind that it eats only human meat)", cat, chernobylPetShop));
-        productDataStore.add(new Product("Snail Cat", new BigDecimal("1000"), "USD", "The snail cat is the slowest animal, but when it sees a laser pointer it becomes the fastest animal in Chernobyl.", cat, chernobylPetShop));
-        productDataStore.add(new Product("Pig Cat", new BigDecimal("6000"), "USD", "Pig cats are just like pigs with just a few differences. Tends to meow and also cleaner.", cat, chernobylPetShop));
-        productDataStore.add(new Product("Bunny", new BigDecimal("890"), "USD", "Funny bunny for sale", smallMammals, smallPetSelect));
-        productDataStore.add(new Product("Iguana", new BigDecimal("470"), "USD", "Amazing rockstar iguanas waiting for you!", reptiles, petSuppliesPlus));
-        productDataStore.add(new Product("Oppossum", new BigDecimal("49.9"), "USD", "Playful oppossums - good choice for children! ", pocketPets, superPetStore));
+            //setting up products and printing it
+            productDataStore.add(new Product("Bear Cat", new BigDecimal("10000"), "USD", "This bear cat is an authentic animal from Chernobyl, might still radiate.", cat, chernobylPetShop));
+            productDataStore.add(new Product("Rabbit Cat", new BigDecimal("3000"), "USD", "The rabbit cat is the nicest little thing caused by the accident. (only if you don't mind that it eats only human meat)", cat, chernobylPetShop));
+            productDataStore.add(new Product("Snail Cat", new BigDecimal("1000"), "USD", "The snail cat is the slowest animal, but when it sees a laser pointer it becomes the fastest animal in Chernobyl.", cat, chernobylPetShop));
+            productDataStore.add(new Product("Pig Cat", new BigDecimal("6000"), "USD", "Pig cats are just like pigs with just a few differences. Tends to meow and also cleaner.", cat, chernobylPetShop));
+            productDataStore.add(new Product("Bunny", new BigDecimal("890"), "USD", "Funny bunny for sale", smallMammals, smallPetSelect));
+            productDataStore.add(new Product("Iguana", new BigDecimal("470"), "USD", "Amazing rockstar iguanas waiting for you!", reptiles, petSuppliesPlus));
+            productDataStore.add(new Product("Oppossum", new BigDecimal("49.9"), "USD", "Playful oppossums - good choice for children! ", pocketPets, superPetStore));
+        }
     }
 }
--- src/main/java/com/codecool/shop/dao/database_implementation/DatabaseManager.java
+++ src/main/java/com/codecool/shop/dao/database_implementation/DatabaseManager.java
@@ -1,15 +1,26 @@
 package com.codecool.shop.dao.database_implementation;
 
+import com.codecool.shop.dao.ProductCategoryDao;
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.SupplierDao;
+import com.codecool.shop.dao.implementation.OrderDaoMem;
+import org.postgresql.ds.PGSimpleDataSource;
+
 import javax.sql.DataSource;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.Objects;
 import java.util.Properties;
 
 public class DatabaseManager {
     private static DatabaseManager databaseManager = null;
     private static Properties properties;
+    private ProductDao productDao;
+    private SupplierDao supplierDataStore;
+    private ProductCategoryDao productCategoryDataStore;
+    private OrderDaoMem orderDataStore;
 
     public static DatabaseManager getInstance() {
         if(databaseManager == null) {
@@ -18,7 +29,27 @@ public class DatabaseManager {
         return databaseManager;
     }
 
-    public void setup() throws IOException {
+    public Properties getProperties() {
+        return properties;
+    }
+
+    public ProductDao getProductDao() {
+        return productDao;
+    }
+
+    public SupplierDao getSupplierDataStore() {
+        return supplierDataStore;
+    }
+
+    public ProductCategoryDao getProductCategoryDataStore() {
+        return productCategoryDataStore;
+    }
+
+    public OrderDaoMem getOrderDataStore() {
+        return orderDataStore;
+    }
+
+    public void setup() throws IOException, SQLException {
         properties = initializeProperties();
         DataSource dataSource = connect(properties);
 
@@ -33,8 +64,22 @@ public class DatabaseManager {
         return properties;
     }
 
-    private DataSource connect(Properties properties){
-        DataSource dataSource = new DataSource
+    private DataSource connect(Properties properties) throws SQLException {
+        PGSimpleDataSource dataSource = new PGSimpleDataSource();
+        String url = properties.getProperty("url");
+        String database = properties.getProperty("database");
+        String user = properties.getProperty("user");
+        String password = properties.getProperty("password");
+        String dao = properties.getProperty("dao");
+
+        dataSource.setDatabaseName(database);
+        dataSource.setUser(user);
+        dataSource.setPassword(password);
+
+        System.out.println("Trying to connect");
+        dataSource.getConnection().close();
+        System.out.println("Connection ok.");
+
 
         return dataSource;
     }

068bd4e3da6976350b2c8e33f4084a44192f1194
takacsberni <takacsberni@gmail.com>
2022-05-17 16:36:58 +0200

    feat: implement add on UserDaoJdbc.java

--- src/main/java/com/codecool/shop/dao/database_implementation/UserDaoJdbc.java
+++ src/main/java/com/codecool/shop/dao/database_implementation/UserDaoJdbc.java
@@ -4,6 +4,7 @@ import com.codecool.shop.dao.UserDao;
 import com.codecool.shop.model.User;
 
 import javax.sql.DataSource;
+import java.sql.*;
 
 public class UserDaoJdbc implements UserDao {
     /** Kiteljesíti a UserDao-ban meghatározottakat: add, find, findByEmail
@@ -16,8 +17,31 @@ public class UserDaoJdbc implements UserDao {
     }
 
     @Override
-    public void add(User user){
+    public void add(User user) throws SQLException {
+        try (Connection connection = dataSource.getConnection()){
+            String query = "INSERT INTO user (email, password, name, phone_number, billing_country, billing_city, " +
+                    "billing_zipcode, billing_address, shipping_country, shipping_city, shipping_zipcode, shipping_address) " +
+                    "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+            PreparedStatement st = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+            st.setString(1, user.getEmail());
+            st.setString(2, user.getPassword());
+            st.setString(3, user.getName());
+            st.setString(4, user.getPhone_number());
+            st.setString(5, user.getBilling_country());
+            st.setString(6, user.getBilling_city());
+            st.setString(7, user.getBilling_zipcode());
+            st.setString(8, user.getBilling_address());
+            st.setString(9, user.getShipping_country());
+            st.setString(10, user.getShipping_city());
+            st.setString(11, user.getShipping_zipcode());
+            st.setString(12, user.getShipping_address());
 
+            st.executeUpdate();
+            ResultSet rs = st.getGeneratedKeys();
+            rs.next();
+        } catch(SQLException e){
+            throw new RuntimeException("Error while adding user.", e);
+        }
     }
 
     @Override

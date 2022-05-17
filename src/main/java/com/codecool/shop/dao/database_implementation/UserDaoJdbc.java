97f522fe142e6d4b277f89bb1f45089135911827
takacsberni <takacsberni@gmail.com>
2022-05-17 14:45:51 +0200

    feat: add UserDaoJdbc and UserService classes

---
+++ src/main/java/com/codecool/shop/dao/database_implementation/UserDaoJdbc.java
@@ -0,0 +1,5 @@
+package com.codecool.shop.dao.database_implementation;
+
+public class UserDaoJdbc {
+    //TODO UserService ezt hívja meg (regisztráció)
+}
---
+++ src/main/java/com/codecool/shop/service/UserService.java
@@ -0,0 +1,5 @@
+package com.codecool.shop.service;
+
+public class UserService {
+    //TODO: regisztrációnál a post ezen keresztül hívja meg a userDaoJdbc-n az add()-ot
+}

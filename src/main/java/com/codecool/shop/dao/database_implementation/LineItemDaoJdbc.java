e8685f9abe9a603d2dfee323cba4343417ca13e2
DianaDob <dobsinszki.diana@gmail.com>
2022-05-17 10:18:56 +0200

    feat: implement Dao-s in Jdbc-s

--- src/main/java/com/codecool/shop/dao/database_implementation/LineItemDaoJdbc.java
+++ src/main/java/com/codecool/shop/dao/database_implementation/LineItemDaoJdbc.java
@@ -1,4 +1,36 @@
 package com.codecool.shop.dao.database_implementation;
 
-public class LineItemDaoJdbc {
+import com.codecool.shop.dao.LineItemDao;
+import com.codecool.shop.model.LineItem;
+
+import javax.sql.DataSource;
+import java.util.List;
+
+public class LineItemDaoJdbc implements LineItemDao {
+
+    DataSource dataSource;
+
+    public LineItemDaoJdbc(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    public void add(LineItem lineItem) {
+
+    }
+
+    @Override
+    public LineItem find(int id) {
+        return null;
+    }
+
+    @Override
+    public void remove(int id) {
+
+    }
+
+    @Override
+    public List<LineItem> getAll() {
+        return null;
+    }
 }

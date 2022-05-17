2c670c0e8b1aff2bd8d5d31005c07f76c30f5b3f
takacsberni <takacsberni@gmail.com>
2022-05-17 14:36:56 +0200

    feat: send data from registration form and redirect to the index.html

--- src/main/java/com/codecool/shop/controller/RegistrationController.java
+++ src/main/java/com/codecool/shop/controller/RegistrationController.java
@@ -1,8 +1,12 @@
 package com.codecool.shop.controller;
 
+import com.codecool.shop.config.TemplateEngineUtil;
 import com.codecool.shop.dao.OrderDao;
 import com.codecool.shop.dao.implementation.OrderDaoMem;
+import com.codecool.shop.model.Order;
 import com.codecool.shop.service.OrderService;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,6 +14,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
 
 @WebServlet(urlPatterns = {"/registration"})
 public class RegistrationController extends HttpServlet {
@@ -18,11 +24,25 @@ public class RegistrationController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+        WebContext context = new WebContext(req, resp, req.getServletContext());
+        engine.process("product/registration.html", context, resp.getWriter());
     }
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response){
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        HashMap<String, String> parameters = new HashMap<>();
+        parameters.put("name", request.getParameter("name"));
+        parameters.put("email", request.getParameter("email"));
+        parameters.put("password", request.getParameter("password"));
 
+        Order order = orderservice.getOrderById(orderservice.getCurrentOrderId());
+        order.setCustomerData(parameters);
+        response.setContentType("text/html");
+        PrintWriter pw=response.getWriter();
+
+        response.sendRedirect("product/index.html");
+
+        pw.close();
     }
 }

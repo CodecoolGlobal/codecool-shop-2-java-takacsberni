b2aa2d6374a296449875601024cdac3d95069617
DianaDob <dobsinszki.diana@gmail.com>
2022-05-18 13:34:34 +0200

    feat: implement add in ProductCategoryDaoJdbc

--- src/main/java/com/codecool/shop/dao/database_implementation/ProductCategoryDaoJdbc.java
+++ src/main/java/com/codecool/shop/dao/database_implementation/ProductCategoryDaoJdbc.java
@@ -4,6 +4,10 @@ import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.model.ProductCategory;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 public class ProductCategoryDaoJdbc implements ProductCategoryDao {
@@ -16,7 +20,16 @@ public class ProductCategoryDaoJdbc implements ProductCategoryDao {
 
     @Override
     public void add(ProductCategory category) {
-
+        try(Connection connection = dataSource.getConnection()){
+            String sql = "INSERT INTO category (name, description, department) VALUES (?, ? ,?)";
+            PreparedStatement st = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            st.setString(1, category.getName());
+            st.setString(2, category.getDescription());
+            st.setString(3, category.getDepartment());
+            st.executeUpdate();
+        } catch (SQLException throwables) {
+            throw new RuntimeException("Error while adding product category", throwables);
+        }
     }
 
     @Override
